4d45fe0b1c645f70ac7f799c3b82f2891f2f84a3
Heatwave <stan_yxr@163.com>
2018-06-17 15:00:15 +0800

    Set up 1st version code

---
+++ index.js
@@ -0,0 +1,88 @@
+// @ts-check
+'use strict';
+
+const cloneDeep = require('lodash.clonedeep');
+
+function checkKeyIsString(key) {
+  if (typeof key !== 'string') throw new TypeError('cache key must be a string');
+}
+
+// use closure and WeakMap for private property
+const Cache = (() => {
+  const _cache = new WeakMap();
+  const _cacheTimeout = new WeakMap();
+  const _options = new WeakMap();
+
+  class Cache {
+
+    /**
+     * Cache constructor
+     * @param {object} [options={}] cache options setting
+     * @param {boolean} [options.deepClone=false] turn on deep clone for cache put or not
+     */
+    constructor(options) {
+      if (!options || typeof options !== 'object') options = {};
+
+      options.deepClone = options.deepClone === true ? true : false;
+
+      _cache.set(this, {});
+      _cacheTimeout.set(this, {});
+      _options.set(this, {
+        deepClone: options.deepClone
+      });
+
+      this.get = this.get.bind(this);
+      this.put = this.put.bind(this);
+      this.del = this.del.bind(this);
+    }
+
+    /**
+     * get a cached value by key
+     * @param {string} key cache key to get the value
+     * @throws {TypeError} when the key is not string
+     */
+    get(key) {
+      checkKeyIsString(key);
+      return _cache.get(this)[key] === undefined ? null : _cache.get(this)[key];
+    }
+
+    /**
+     * put a value to the cache by key
+     * @param {string} key cache key to save the value to cache
+     * @param {*} value the value want to cached
+     * @param {number} [ttl] time to live, the cache should be delete after this millisecond, if not given, keep the cache forever
+     * @throws {TypeError} when the key is not string, or ttl is not number
+     */
+    put(key, value, ttl) {
+      checkKeyIsString(key);
+
+      if (_options.get(this).deepClone) {
+        _cache.get(this)[key] = cloneDeep(value);
+      } else {
+        _cache.get(this)[key] = value;
+      }
+
+      clearTimeout(_cacheTimeout.get(this)[key]);
+
+      if (typeof ttl === 'number') {
+        _cacheTimeout.get(this)[key] = setTimeout(() => {
+          _cache.get(this)[key] = null;
+        }, ttl);
+      }
+    }
+
+    /**
+     * delete a cached value by key
+     * @param {string} key cache key to delete the cache
+     * @throws {TypeError} when the key is not string
+     */
+    del(key) {
+      checkKeyIsString(key);
+      _cache.get(this)[key] = null;
+    }
+  }
+
+  return Cache;
+})();
+
+module.exports = Cache;
---
+++ index.test.js
@@ -0,0 +1,126 @@
+// @ts-check
+'use strict';
+
+const Cache = require('./index.js');
+const should = require('chai').should();
+
+describe('Cache', () => {
+  describe('basic', () => {
+    /**
+     * @type {Cache}
+     */
+    let cache;
+
+    beforeEach(() => {
+      cache = new Cache();
+    });
+
+    it('should put a value and get it success', () => {
+      cache.put('test', 1);
+      should.equal(cache.get('test'), 1);
+    });
+
+    it('should return null if do not hit the cache', () => {
+      should.equal(null, cache.get('qweasdzxc'));
+    });
+
+    it('should delete cache success', () => {
+      cache.put('test', 1);
+      cache.del('test');
+      should.equal(null, cache.get('test'));
+    });
+
+    it('should accept a string as get method key', () => {
+      // @ts-ignore
+      (() => cache.get(1)).should.throw(TypeError);
+    });
+
+    it('should accept a string as put method key', () => {
+      // @ts-ignore
+      (() => cache.put(1)).should.throw(TypeError);
+    });
+
+    it('should accept a string as del method key', () => {
+      // @ts-ignore
+      (() => cache.del(1)).should.throw(TypeError);
+    });
+
+  });
+
+  describe('cache with ttl', () => {
+    /**
+     * @type {Cache}
+     */
+    let cache;
+
+    beforeEach(() => {
+      cache = new Cache();
+    });
+
+    it('should be null after ttl', done => {
+      cache.put('test', 1, 100);
+      should.equal(cache.get('test'), 1);
+      setTimeout(() => {
+        should.equal(null, cache.get('test'));
+        done();
+      }, 200);
+    });
+
+    it('should clear timeout after put new value', done => {
+      cache.put('test', 1, 100);
+      cache.put('test', 2);
+      setTimeout(() => {
+        should.equal(2, cache.get('test'));
+        done();
+      }, 200);
+    });
+
+  });
+
+  describe('with no deepClone enabled', () => {
+    /**
+     * @type {Cache}
+     */
+    let cache;
+
+    beforeEach(() => {
+      cache = new Cache();
+    });
+
+    it('should save the reference', () => {
+      let obj = {
+        a: 1
+      };
+      cache.put('test', obj);
+      should.equal(1, cache.get('test').a);
+      obj.a = 2;
+      should.equal(2, cache.get('test').a);
+    });
+
+  });
+
+  describe('with deepClone is true', () => {
+
+    /**
+     * @type {Cache}
+     */
+    let cache;
+
+    beforeEach(() => {
+      cache = new Cache({
+        deepClone: true
+      });
+    });
+
+    it('should save the clone obj', () => {
+      let obj = {
+        a: 1
+      };
+      cache.put('test', obj);
+      should.equal(1, cache.get('test').a);
+      obj.a = 2;
+      should.equal(1, cache.get('test').a);
+    });
+
+  });
+});
